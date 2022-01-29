2de75ce44eb0bad360dc63b6eb75f6e8cf3a150f
Dmitriy Maslakov <dmitry.maslakov@gmail.com>
2022-01-30 01:15:05 +0200

    les 4

--- functions.php
+++ functions.php
@@ -1,5 +1,30 @@
 <?php
 
+function addUser($email, $password)
+{
+    $dbh = connectDb();
+    // готовим запрос в БД
+    $sql = 'INSERT INTO users (email, password) VALUES (:email, :password)';
+    $sth = $dbh->prepare($sql);
+    // Выполняем запрос:
+    $sth->execute(['email' => $email, 'password' => password_hash($password, PASSWORD_DEFAULT)]);
+    //    $sth->bindParam(':emaile', $email);
+    //    $sth->bindParam(':password', $hashedPassword);
+    //    $sth->execute();
+    // готовим флеш-сообщение
+}
+
+function connectDb()
+{
+    // Создаем объект-соединение с базой данных
+    $dbh = new \PDO(
+        'mysql:host=localhost;dbname=test',
+        'root',
+        ''
+    );
+    return $dbh;
+}
+
 function displayFlashMassage($name)
 {
     if(isset($_SESSION[$name])) {
@@ -12,12 +37,7 @@ function displayFlashMassage($name)
 
 function getUserByEmail($email)
 {
-    // Создаем объект-соединение с базой данных
-    $dbh = new \PDO(
-        'mysql:host=localhost;dbname=test',
-        'root',
-        ''
-    );
+    $dbh = connectDb();
     // Готовим запрос
     $sql = 'SELECT * FROM users WHERE email=:email';
     $sth = $dbh->prepare($sql);
@@ -31,24 +51,20 @@ function getUserByEmail($email)
     return $user;
 }
 
-
-function addUser($email, $password)
+function geUsersList()
 {
-    // Создаем объект-соединение с базой данных
-    $dbh = new \PDO(
-        'mysql:host=localhost;dbname=test',
-        'root',
-        ''
-    );
-    // готовим запрос в БД
-    $sql = 'INSERT INTO users (email, password) VALUES (:email, :password)';
+    $dbh = connectDb();
+    // Готовим запрос
+    $sql = 'SELECT * FROM users';
     $sth = $dbh->prepare($sql);
+
     // Выполняем запрос:
-    $sth->execute(['email' => $email, 'password' => password_hash($password, PASSWORD_DEFAULT)]);
-    //    $sth->bindParam(':emaile', $email);
-    //    $sth->bindParam(':password', $hashedPassword);
-    //    $sth->execute();
-    // готовим флеш-сообщение
+    $sth->execute();
+
+    // Получаем данные результата запроса:
+    $users = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+    return $users;
 }
 
 function redirectTo($path)
--- login.php
+++ login.php
@@ -5,7 +5,8 @@ require 'functions.php';
 // take email and password
 $email = $_POST['email'];
 $password = $_POST['password'];
-$rememberme = $_POST['rememberme']; // TO DO если есть создать куку
+if(isset($_POST['rememberme']))
+    {$rememberme = $_POST['rememberme'];} // TO DO если есть создать куку
 
 // 1. проверяем наличие email в БД
 // 2. получаем из БД email и password
@@ -16,10 +17,14 @@ $login = password_verify($password, $user['password']);
 
 // 4. если проверка прошла авторизуем и отправляем на страницу users
 if($login && !empty($user)) {
+    //$_SESSION['email'] = $_POST['email'];
+    $_SESSION['user'] = $user;
+    $_SESSION['login'] = $login;
+    $_SESSION['email'] = $email;
     redirectTo('users.php');
+    exit();
 // 5. если нет - выводим сообщение и отправляем на повторную авторизацию
 }else{
     setFlashMassage('danger', 'Логин или пароль введены неверно');
     redirectTo('page_login.php');
-    exit();
 }
