b9596dc8e232a4d58e732a417cc38fafddb89203
Dmitriy Maslakov <dmitry.maslakov@gmail.com>
2022-02-04 15:21:55 +0200

    Исправлены ошибки post вместо get и delete from

--- delete.php
+++ delete.php
@@ -2,19 +2,13 @@
 session_start();
 require 'functions.php';
 
-
-if (!isset($_POST) && !isset($_SESSION['id'])) {
-    redirectTo('users.php');
-    exit();
-}
 $id = $_GET['id'];
 
-if (isset($_POST['id'])) {
+if (isset($_GET['id'])) {
     deleteUser($id);
     setFlashMassage('success', 'Данные обновлены');
     // TO DO удалить аватар с сервера
-    redirectTo('users.php');
 }else{
     setFlashMassage('danger', 'Что-то пошло не так');
-    redirectTo('media.php');
 }
+redirectTo('users.php');
